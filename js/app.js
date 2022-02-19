19caecc62ebf6a8044fa090ff84d26b05ea2c13f
Isaac <uniqucode@gmail.com>
2022-02-19 14:12:58 +0100

    Added e.preventDefault to prevent auto-refreshing of the page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const date = document.querySelector('.date');
 const addForm = document.querySelector('.new-todo');
 
+addForm.addEventListener('submit', e => {
+e.preventDefault();
+// const todo = addForm.new-todo.value.trim();
+
+
+})
 
 // SHOW TODAY'S DATE
 const options = {weekday: 'long', month: 'short', day: 'numeric'};
