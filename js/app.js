0e3903f976f51bc1731326ab1dfdbd6f94ac716f
Isaac <uniqucode@gmail.com>
2022-02-19 16:51:21 +0100

    Created a reusable function

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 const date = document.querySelector('.date');
 const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+
+    const html = `
+    <div class="content">
+              <input type="text" class="text" value="${todo}" readonly />
+              <i class="far fa-trash-alt delete"></i>
+            </div>
+    `
+    list.innerHTML += html;
+}
 
 addForm.addEventListener('submit', e => {
 e.preventDefault();
 const todo = addForm.add.value.trim();
 
-console.log(todo);
+
+// if(todo.length) {
+//     generateTemplate(todo);
+//     addForm.reset();
+// }
 
 });
 
